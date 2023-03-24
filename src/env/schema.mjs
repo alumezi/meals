fafda0c51b292b9bb9b9fec6613d294053e53968
alumezi <arbnorlumezi@gmail.com>
2023-03-24 14:21:49 +0100

    Cleanup.

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -19,8 +19,6 @@ export const serverSchema = z.object({
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string() : z.string().url()
   ),
-  INSTAGRAM_CLIENT_ID: z.string(),
-  INSTAGRAM_CLIENT_SECRET: z.string(),
 });
 
 /**
@@ -33,8 +31,6 @@ export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  INSTAGRAM_CLIENT_ID: process.env.INSTAGRAM_CLIENT_ID,
-  INSTAGRAM_CLIENT_SECRET: process.env.INSTAGRAM_CLIENT_SECRET,
 };
 
 /**
