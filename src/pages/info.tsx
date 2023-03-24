0c9d1f4e13a3abef71b37b0297f271649d534836
alumezi <arbnorlumezi@gmail.com>
2023-03-24 22:05:34 +0100

    Add card

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -9,40 +9,65 @@ const Info = () => {
   return (
     //full width tailwind css
     <div className="carousel-center carousel h-screen w-screen space-x-4 p-4 ">
-      <div className="carousel-item w-11/12 max-w-md">
-        <Image
-          alt="meal-plan"
-          src={food1}
-          className="rounded-box object-cover "
-        />
+      <div className="card-compact carousel-item card mb-12  w-11/12 max-w-md bg-base-100 shadow-xl">
+        <figure className="h-1/2">
+          <Image alt="meal-plan" src={food1} className="h-full object-cover " />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn-primary btn">Buy Now</button>
+          </div>
+        </div>
       </div>
-      <div className="carousel-item w-11/12 max-w-md">
-        <Image
-          alt="meal-plan"
-          src={food2}
-          className="rounded-box object-cover"
-        />
+      <div className="card-compact carousel-item card mb-12 w-11/12 max-w-md bg-base-100 shadow-xl">
+        <figure className="h-1/2">
+          <Image alt="meal-plan" src={food2} className="h-full object-cover " />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn-primary btn">Buy Now</button>
+          </div>
+        </div>
       </div>
-      <div className="carousel-item w-11/12 max-w-md">
-        <Image
-          alt="meal-plan"
-          src={food4}
-          className="rounded-box object-cover"
-        />
+      <div className="card-compact carousel-item card mb-12 w-11/12 max-w-md bg-base-100 shadow-xl">
+        <figure className="h-1/2">
+          <Image alt="meal-plan" src={food4} className="h-full object-cover " />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn-primary btn">Buy Now</button>
+          </div>
+        </div>
       </div>
-      <div className="carousel-item w-11/12 max-w-md">
-        <Image
-          alt="meal-plan"
-          src={food5}
-          className="rounded-box object-cover"
-        />
+      <div className="card-compact carousel-item card mb-12 w-11/12 max-w-md bg-base-100 shadow-xl">
+        <figure className="h-1/2">
+          <Image alt="meal-plan" src={food5} className="h-full object-cover " />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn-primary btn">Buy Now</button>
+          </div>
+        </div>
       </div>
-      <div className="carousel-item w-11/12 max-w-md">
-        <Image
-          alt="meal-plan"
-          src={food6}
-          className="rounded-box object-cover"
-        />
+      <div className="card-compact carousel-item card mb-12 w-11/12 max-w-md bg-base-100 shadow-xl">
+        <figure className="h-1/2">
+          <Image alt="meal-plan" src={food6} className="h-full object-cover " />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn-primary btn">Buy Now</button>
+          </div>
+        </div>
       </div>
     </div>
   );
