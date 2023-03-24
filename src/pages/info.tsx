6e83ce18f7da684bbc3b4dd5ee611b0f1af32f9b
alumezi <arbnorlumezi@gmail.com>
2023-03-24 21:40:30 +0100

    Add images.

---
+++ src/pages/info.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import food1 from "src/assets/images/food1.webp";
+import food2 from "src/assets/images/food2.jpg";
+import food4 from "src/assets/images/food4.webp";
+import food5 from "src/assets/images/food5.jpg";
+import food6 from "src/assets/images/food6.jpg";
+
+const Info = () => {
+  return (
+    //full width tailwind css
+    <div className="carousel-center carousel h-screen w-full space-x-4 p-4">
+      <div className="carousel-item max-w-md">
+        <Image
+          alt="meal-plan"
+          src={food1}
+          className="rounded-box object-cover "
+        />
+      </div>
+      <div className="carousel-item max-w-md">
+        <Image
+          alt="meal-plan"
+          src={food2}
+          className="rounded-box object-cover"
+        />
+      </div>
+      <div className="carousel-item max-w-md">
+        <Image
+          alt="meal-plan"
+          src={food4}
+          className="rounded-box object-cover"
+        />
+      </div>
+      <div className="carousel-item max-w-md">
+        <Image
+          alt="meal-plan"
+          src={food5}
+          className="rounded-box object-cover"
+        />
+      </div>
+      <div className="carousel-item max-w-md">
+        <Image
+          alt="meal-plan"
+          src={food6}
+          className="rounded-box object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Info;
