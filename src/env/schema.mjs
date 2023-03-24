cb9fccefc4e1bbc0936b7bc45f071374ebf5e8b5
alumezi <arbnorlumezi@gmail.com>
2023-03-24 14:40:54 +0100

    Fix client vars.

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -19,6 +19,8 @@ export const serverSchema = z.object({
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string() : z.string().url()
   ),
+  FACEBOOK_ID: z.string(),
+  FACEBOOK_SECRET: z.string(),
 });
 
 /**
@@ -31,6 +33,8 @@ export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
+  FACEBOOK_ID: process.env.FACEBOOK_ID,
 };
 
 /**
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import NextAuth, { type Session, type NextAuthOptions } from "next-auth";
-import InstagramProvider from "next-auth/providers/instagram";
 import FacebookProvider from "next-auth/providers/facebook";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
-// import { prisma } from "../../../server/db";
+import { prisma } from "../../../server/db";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -16,11 +14,11 @@ export const authOptions: NextAuthOptions = {
     },
   },
   // Configure one or more authentication providers
-  // adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID || "",
-      clientSecret: process.env.FACEBOOK_SECRET || "",
+      clientId: env.FACEBOOK_ID,
+      clientSecret: env.FACEBOOK_SECRET,
     }),
   ],
 };
